811bd31778d278eb1836d0a04a15edf07bf16221
Bow-cyber <rokeyabegum2029@gmail.com>
2025-06-06 22:33:42 +0600

    Update 00_autodowninsta.js

--- scripts/cmds/00_autodowninsta.js
+++ scripts/cmds/00_autodowninsta.js
@@ -1,94 +1,45 @@
-const fs = require("fs-extra");
 const axios = require("axios");
-
 module.exports = {
+  config: {
+    name: "dl",
+    aliases: ["download"],
+    version: "1.0",
+    author: " Mr.Smokey🚬 ",
+    countDown: 2,
+    role: 0,
+    description: {
+      en: "Download 1000+ website's videos",
+    },
+    category: "media",
+    guide: {
+      en: "{pn} [url | reply to an url]",
+    },
+  },
 
-	threadStates: {},
+  onStart: async function ({ api, args, event }) {
+    const url = event.messageReply?.body || args[0];
+       if (!url) {
+               api.setMessageReaction("❌", event.messageID, () => {}, true);
+              return api.sendMessage("⁉️ | Please provide a valid URL Or reply to an URL", event.threadID, event.messageID);
+    }
+    try {
+    const { data } = await axios.get("https://raw.githubusercontent.com/KingsOfToxiciter/alldl/refs/heads/main/toxicitieslordhasan.json");
+    const hasan = data.hasan;
+      
+    const downloadLink = `${hasan}/alldl?url=${encodeURIComponent(url)}`;
 
-	config: {
-		name: 'autoinsta',
-		version: '1.0',
-		author: 'Kshitiz',
-		countDown: 5,
-		role: 0,
-		shortDescription: 'auto video downloader',
-		longDescription: '',
-		category: 'media',
-		guide: {
-			en: '{p}{n}',
-		}
-	},
-	onStart: async function ({ api, event }) {
-		const threadID = event.threadID;
+        api.sendMessage(
+          {
+            body: "✨ | Here is your Download video..!!",
+            attachment: await global.utils.getStreamFromURL(downloadLink)
+          },
+          event.threadID,
+          event.messageID
+        );
 
-
-		if (!this.threadStates[threadID]) {
-			this.threadStates[threadID] = {
-				autoInstaEnabled: false,
-			};
-		}
-
-
-		if (event.body.toLowerCase().includes('autoinsta')) {
-
-			if (event.body.toLowerCase().includes('on')) {
-
-				this.threadStates[threadID].autoInstaEnabled = true;
-				api.sendMessage("AutoInsta is now ON.", event.threadID, event.messageID);
-			} else if (event.body.toLowerCase().includes('off')) {
-
-				this.threadStates[threadID].autoInstaEnabled = false;
-				api.sendMessage("AutoInsta is now OFF.", event.threadID, event.messageID);
-			} else {
-
-				api.sendMessage("type 'autoinsta on' to turn on and\n'autoinsta off' to turn off.", event.threadID, event.messageID);
-			}
-		}
-	},
-	onChat: async function ({ api, event }) {
-		const threadID = event.threadID;
-
-
-		if (this.threadStates[threadID] && this.threadStates[threadID].autoInstaEnabled && this.checkLink(event.body)) {
-			var { url } = this.checkLink(event.body);
-			this.downLoad(url, api, event);
-			api.setMessageReaction("?", event.messageID, (err) => {}, true);
-		}
-	},
-	downLoad: function (url, api, event) {
-		var time = Date.now();
-		var path = __dirname + `/cache/${time}.mp4`;
-		this.getLink(url).then(res => {
-			axios({
-				method: "GET",
-				url: res,
-				responseType: "arraybuffer"
-			}).then(res => {
-				fs.writeFileSync(path, Buffer.from(res.data, "utf-8"));
-				if (fs.statSync(path).size / 1024 / 1024 > 25) {
-					return api.sendMessage("The file is too large, cannot be sent", event.threadID, () => fs.unlinkSync(path), event.messageID);
-				}
-				api.sendMessage({
-					body: "Successful Download!",
-					attachment: fs.createReadStream(path)
-				}, event.threadID, () => fs.unlinkSync(path), event.messageID);
-			}).catch(err => console.error(err));
-		}).catch(err => console.error(err));
-	},
-	getLink: function (url) {
-		return new Promise((resolve, reject) => {
-			axios({
-				method: "GET",
-				url: `https://for-devs.rishadapis.repl.co/api/instadl?url=${url}&apikey=fuck`
-			}).then(res => resolve(res.data.video)).catch(err => reject(err));
-		});
-	},
-	checkLink: function (url) {
-		if (url.includes("instagram")) {
-			return {
-				url: url
-			};
-		}
-		return null;
-	}
+    } catch (error) {
+      api.setMessageReaction("❎", event.messageID, () => {}, true);
+      api.sendMessage(`❌ | Error:\n${error.message}`, event.threadID, event.messageID);
+    }
+  },
 };
